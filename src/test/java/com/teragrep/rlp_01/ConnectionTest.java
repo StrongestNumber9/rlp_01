22562b21d9cf219577cd29035f44ccdae2c97289
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2023-10-11 12:05:38 +0300

    Kill the server as well

--- src/test/java/com/teragrep/rlp_01/ConnectionTest.java
+++ src/test/java/com/teragrep/rlp_01/ConnectionTest.java
@@ -15,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
 public class ConnectionTest implements Runnable {
-    private ServerSocket serverSocket;
     private Selector selector;
     private Thread thread;
 
@@ -25,7 +24,6 @@ public class ConnectionTest implements Runnable {
         System.setProperty("syslog.server.host", "127.0.0.1");
         System.setProperty("syslog.server.port", "1236");
         System.setProperty("syslog.server.protocol", "TCP");
-        serverSocket = new ServerSocket(1236);
         selector = Selector.open();
         this.thread.start();
     }
@@ -40,20 +38,28 @@ public class ConnectionTest implements Runnable {
         String reply ="200 OK\nrelp_version=0\nrelp_software=RLP-01,1.0.1,https://teragrep.com\ncommands=syslog";
         try {
             // Handle first connect
-            Socket socket = this.serverSocket.accept();
+            ServerSocket serverSocket = new ServerSocket(1236);
+            Socket socket = serverSocket.accept();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String line;
             while((line=bufferedReader.readLine())!=null && !line.isEmpty());
+            // Server opening message
             socket.getOutputStream().write(("1 rsp " + reply.length() + " " + reply + "\n").getBytes());
-            socket.getInputStream().read();
+            // Kill server immediately after sending message causing commit to fail
+            socket.close();
+            serverSocket.close();
 
             // Handle second connect
-            socket = this.serverSocket.accept();
+            serverSocket = new ServerSocket(1236);
+            socket = serverSocket.accept();
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             while((line=bufferedReader.readLine())!=null && !line.isEmpty());
+            // Server opening message
             socket.getOutputStream().write(("1 rsp " + reply.length() + " " + reply + "\n").getBytes());
             socket.getInputStream().read();
+            // First RelpBatch message
             socket.getOutputStream().write("2 rsp 6 200 OK\n".getBytes());
+            // Disconnection message
             socket.getOutputStream().write("3 rsp 6 200 OK\n".getBytes());
             serverSocket.close();
         } catch (IOException e) {
@@ -64,10 +70,21 @@ public class ConnectionTest implements Runnable {
     @Test
     public void testTeardown() throws IOException, TimeoutException {
         RelpConnection relpConnection = new RelpConnection();
+        // First connection
         relpConnection.connect("127.0.0.1", 1236);
-        relpConnection.tearDown();
-        relpConnection.connect("127.0.0.1", 1236);
+
+        // Server socket is closed by this point
         RelpBatch relpBatch = new RelpBatch();
+        relpBatch.insert("Sending a message".getBytes(StandardCharsets.UTF_8));
+        try {
+            relpConnection.commit(relpBatch);
+        } catch(IOException ignored) {
+            relpConnection.tearDown();
+        }
+
+        // New server socket is avaialble for second connection
+        relpConnection.connect("127.0.0.1", 1236);
+        relpBatch = new RelpBatch();
         relpBatch.insert("Sending a message".getBytes(StandardCharsets.UTF_8));
         relpConnection.commit(relpBatch);
         relpConnection.disconnect();
